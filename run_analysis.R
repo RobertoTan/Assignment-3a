2680632657437f29cda3f0b456ed05af8daf0108
roberto.tan@wph.com.sg <roberto.tan@wph.com.sg>
2014-10-26 04:14:59 +0800

    run_analysis.R
    Updates

--- run_analysis.R
+++ run_analysis.R
@@ -3,18 +3,18 @@ activityType = read.table('./activity_labels.txt',header=FALSE); #imports activi
 subjectTrain = read.table('./train/subject_train.txt',header=FALSE); #imports subject_train.txt
 xTrain = read.table('./train/x_train.txt',header=FALSE); #imports x_train.txt
 yTrain = read.table('./train/y_train.txt',header=FALSE); #imports y_train.txt
-# Assigin column names to the data imported above
+# This is for assigning column names to the data imported above
 colnames(activityType) = c('activityId','activityType');
 colnames(subjectTrain) = "subjectId";
 colnames(xTrain) = features[,2];
 colnames(yTrain) = "activityId";
-# cCreate the final training set by merging yTrain, subjectTrain, and xTrain
+# This is to create the final training set by merging yTrain, subjectTrain, and xTrain
 trainingData = cbind(yTrain,subjectTrain,xTrain);
 # Read in the test data
 subjectTest = read.table('./test/subject_test.txt',header=FALSE); #imports subject_test.txt
 xTest = read.table('./test/x_test.txt',header=FALSE); #imports x_test.txt
 yTest = read.table('./test/y_test.txt',header=FALSE); #imports y_test.txt
-# Assign column names to the test data imported above
+# IMPORTANT.  Assign column names to the test data imported above
 colnames(subjectTest) = "subjectId";
 colnames(xTest) = features[,2];
 colnames(yTest) = "activityId";
@@ -22,20 +22,19 @@ colnames(yTest) = "activityId";
 testData = cbind(yTest,subjectTest,xTest);
 # Combine training and test data to create a final data set
 finalData = rbind(trainingData,testData);
-# Create a vector for the column names from the finalData, which will be used
-# to select the desired mean() & stddev() columns
+# Create a vector for the column names from the finalData, which will be used to select the desired mean() & stddev() columns
 colNames = colnames(finalData);
-# 2. Extract only the measurements on the mean and standard deviation for each measurement.
-# Create a logicalVector that contains TRUE values for the ID, mean() & stddev() columns and FALSE for others
+# Extract only the measurements on the mean and standard deviation for each measurement.
+# To Create a logicalVector that contains TRUE values for the ID, mean() & stddev() columns and FALSE for others
 logicalVector = (grepl("activity..",colNames) | grepl("subject..",colNames) | grepl("-mean..",colNames) & !grepl("-meanFreq..",colNames) & !grepl("mean..-",colNames) | grepl("-std..",colNames) & !grepl("-std()..-",colNames));
 # Subset finalData table based on the logicalVector to keep only desired columns
 finalData = finalData[logicalVector==TRUE];
-# 3. Use descriptive activity names to name the activities in the data set
+# Use descriptive activity names to name the activities in the data set
 # Merge the finalData set with the acitivityType table to include descriptive activity names
 finalData = merge(finalData,activityType,by='activityId',all.x=TRUE);
 # Updating the colNames vector to include the new column names after merge
 colNames = colnames(finalData);
-# 4. Appropriately label the data set with descriptive activity names.
+# Appropriately label the data set with descriptive activity names.
 # Cleaning up the variable names
 for (i in 1:length(colNames))
 {
@@ -54,7 +53,7 @@ for (i in 1:length(colNames))
 };
 # Reassigning the new descriptive column names to the finalData set
 colnames(finalData) = colNames;
-# 5. Create a second, independent tidy data set with the average of each variable for each activity and each subject.
+# Create a second, independent tidy data set with the average of each variable for each activity and each subject.
 # Create a new table, finalDataNoActivityType without the activityType column
 finalDataNoActivityType = finalData[,names(finalData) != 'activityType'];
 # Summarizing the finalDataNoActivityType table to include just the mean of each variable for each activity and each subject
